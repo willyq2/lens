e564ac0089fd72c965f9265f4d145ec7421cd22e
william webster <94489486+willyq2@users.noreply.github.com>
2022-10-22 16:37:31 -0700

    new website!

---
+++ testing/testing.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("full_page_nav").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("full_page_nav").style.display = "none";
+}
