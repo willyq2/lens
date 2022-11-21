4d7271d2acaf9e70e865a270c48da4e2b754445b
william webster <94489486+willyq2@users.noreply.github.com>
2022-11-21 14:05:41 -0800

    trying jquery

--- testing.js
+++ testing.js
@@ -5,3 +5,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("full_page_nav").style.display = "none";
 }
+
+var lastScrollTop = 0;
+
+$(window).scroll(function () {
+    var st = $(this).scrollTop();
+    if (st < lastScrollTop){
+            $('.name').FadeOut();
+    } else {
+        $('.name').FadeIn();
+    }
+    lastScrollTop = st;
+})
