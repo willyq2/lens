e03e5e8715ac295099e0d14b88d502f67a2254bb
william webster <94489486+willyq2@users.noreply.github.com>
2022-11-22 15:30:06 -0800

    No glitch for disappearing name on scroll

--- testing.js
+++ testing.js
@@ -6,11 +6,56 @@ function closeNav() {
     document.getElementById("full_page_nav").style.display = "none";
 }
 
-$(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    var height = $(window).height();
+function hide(el) {
+    el.style.visibility = 'hidden';
+    return el;
+}
 
-    $('.name').css({
-        'opacity': ((height - scrollTop) / (height))
+function show(el) {
+    el.style.visibility = 'visible';
+    return el;
+}
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+
+        if ($(window).width() > $(window).height()) {
+
+            var scrollTop = $(window).scrollTop();
+            var height = $(window).height();
+
+            var opacity = (height - scrollTop) / (height);
+
+            $('.name').css({
+                'opacity': opacity
+            });
+
+            if (opacity <= 0) {
+                hide($(".name")[0]);
+                hide($(".name")[1]);
+            }
+            if (opacity > 0) {
+                show($(".name")[0]);
+                show($(".name")[1]);
+            }
+        }
+        else {
+            $('.name').css({
+                'opacity': 1
+            });
+        }
+
     });
+
+    $(window).resize(function() {
+        if ($(window).width() < $(window).height()) {
+            $('.name').css({
+                'opacity': 1
+            });
+
+            $(".name").show();
+        }
+    });
+
 });
